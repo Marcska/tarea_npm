196131cb029281c9825c0a69b9350fef3490b004
Marcska <marcska97@gmail.com>
2020-05-12 00:39:18 -0500

    actualizacion ultimo cambio a mi archivo index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const app = express();
+app.get('/', function (req, res) {
+  res.send('Mi primer servidor')
+})
 app.listen(3000, () => console.log('Mi primer servidor por http://localhost:3000'));
