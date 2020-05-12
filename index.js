676b29a1360c6e73c296020153e315243c8cefbb
Marcska <marcska97@gmail.com>
2020-05-12 00:35:54 -0500

    Se actualizo de nuevo el index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Mi primer server corriendo en http://localhost:3000')
-})
-
-app.listen(3000)
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log('Mi primer servidor por http://localhost:3000'));
